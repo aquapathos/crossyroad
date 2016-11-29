066a763a9728bd4c5b96307b4462e074b880415f
aquapathos <firedwater@gmail.com>
2016-11-30 00:38:35 +0900

    Second

--- Assets/Crossy Road/Scripts/AnimatorController.cs
+++ Assets/Crossy Road/Scripts/AnimatorController.cs
@@ -3,5 +3,32 @@ using System.Collections;
 
 public class AnimatorController : MonoBehaviour {
 
+	public PlayerController playerController = null;
+	private Animator animator = null;
 
+	void Start () {
+		animator =  this.GetComponent<Animator>();
+	}
+
+	void Update () {
+		if( playerController.isDead )
+		{
+			animator.SetBool ( "dead", true );
+		}
+		if ( playerController.jumpStart )
+		{
+			animator.SetBool ( "jumpStart", true );
+		}
+		else if ( playerController.isJumping )
+		{
+			animator.SetBool( "jump", true );
+		}
+		else
+		{
+			animator.SetBool ( "jump", false );
+			animator.SetBool ( "jumpStart", false );
+		}
+
+
+	}
 }
