28875e486544334c1b0f986961fe4e1d45daccb4
aquapathos <an>
2016-12-04 13:04:47 +0900

    6th
    コインの動きまで

---
+++ Assets/Crossy Road/Scripts/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	public int coinValue = 1;
+
+	void OnTriggerEnter ( Collider other )
+	{
+		if (other.tag == "Player") {
+			Debug.Log ("Player picked up a coin!");
+
+			// TO DO: Manager -> update coin count 
+
+			Destroy (this.gameObject);
+		}
+	}
+}
